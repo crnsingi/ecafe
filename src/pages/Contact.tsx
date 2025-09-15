e62c321b6dccaae58e082c43a109426ac0e574dc
Cesar <cesarnsingi@hotmail.com>
2025-09-15 14:55:59 +0100

    number update

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -9,7 +9,7 @@ const Contact: React.FC = () => {
         <h1>Contact Us</h1>
         <p>Address: Av. Hoji ya Henda 132, Luanda</p>
         <p>Email: example@domain.com</p>
-        <p>Phone: +244 123 456 789</p>
+        <p>Phone: +244 921 251 783</p>
       </div>
     </div>
   );
